f764f12ab618735263216ce9f1110e1d5ff239ac
Dakota Brown <dakota.kae.brown@gmail.com>
2022-06-22 15:07:54 -0600

    Add in individual item endpoint

--- nms_blueprint_calculator/server.py
+++ nms_blueprint_calculator/server.py
@@ -3,29 +3,27 @@ from json import load
 import pathlib
 
 app = Flask(__name__)
-with open(pathlib.Path(__file__).parent.resolve() / 'static/blueprints.json', 'r') as f:
+with open(pathlib.Path(__file__).parent.resolve() / "static/blueprints.json", "r") as f:
     blueprints = load(f)
 
 
-@app.route('/')
+@app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
 
 
-@app.route('/swagger')
+@app.route("/swagger")
 def swagger_ui():
-    return render_template('swagger.html')
+    return render_template("swagger.html")
 
 
-@app.route('/swagger.json')
+@app.route("/swagger.json")
 def swagger_json():
-    return render_template('swagger.json')
+    return render_template("swagger.json")
 
 
-@app.route('/api/blueprints/<string:category>')
-@app.route('/api/blueprints/<string:category>/')
-@app.route('/api/blueprints', defaults={'category': None})
-@app.route('/api/blueprints/', defaults={'category': None})
+@app.route("/api/blueprints/<string:category>")
+@app.route("/api/blueprints", defaults={"category": None})
 def show_blueprints(category):
     if not category:
         return jsonify(blueprints)
@@ -33,8 +31,53 @@ def show_blueprints(category):
         try:
             return jsonify(blueprints[category])
         except KeyError:
-            return jsonify({'error': {'error_code': 404, 'error_message': f"Category not found: {category}"}}), 404
+            return (
+                jsonify(
+                    {
+                        "error": {
+                            "error_code": 404,
+                            "error_message": f"Category not found: {category}",
+                        }
+                    }
+                ),
+                404,
+            )
 
 
-if __name__ == '__main__':
-    app.run('0.0.0.0', debug=False)
+@app.route("/api/blueprints/<string:category>/<string:item>")
+def show_item_blueprint(category, item):
+    if category not in blueprints:
+        return (
+            jsonify(
+                {
+                    "error": {
+                        "error_code": 404,
+                        "error_message": f"Category not found: {category}",
+                    }
+                }
+            ),
+            404,
+        )
+    elif item not in blueprints[category]:
+        return (
+            jsonify(
+                {
+                    "error": {
+                        "error_code": 404,
+                        "error_message": f"Item not found: {category}/{item}",
+                    }
+                }
+            ),
+            404,
+        )
+    else:
+        return jsonify(blueprints[category][item])
+
+
+@app.route("/api/blueprints/categories")
+def show_categories():
+    return jsonify(list(blueprints.keys()))
+
+
+if __name__ == "__main__":
+    app.run("0.0.0.0", debug=False)
